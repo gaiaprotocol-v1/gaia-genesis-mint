0a71c5d5d572ce1e3a645b676a38fb7a2fc9e8cb
dilrong <dilrong@dilrong.com>
2022-02-03 13:56:08 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gaia Protocol Landing
+
+A decentralized mining type NFT with Kronos DAO.
+
+## Get Started
+
+```
+npm install
+npm run webpack-dev
+cd docs
+swserver
+```
---
+++ src/view/Home.ts
@@ -0,0 +1,25 @@
+import { BodyNode, DomNode, el } from "@hanul/skynode";
+import { View, ViewParams } from "skyrouter";
+
+export default class Landing implements View {
+
+  private container: DomNode;
+
+  constructor() {
+    document.title = "Gaia Protocol";
+
+    BodyNode.append(
+      (this.container = el(".home-view",
+        el("h1", "Gaia Protocol"),
+        el("img.earth", { src: "images/earth.png", alt: "earth" }),
+        el("button", "Mint Your God")
+      ))
+    );
+  }
+
+  public changeParams(params: ViewParams, uri: string): void { }
+
+  public close(): void {
+    this.container.delete();
+  }
+}
